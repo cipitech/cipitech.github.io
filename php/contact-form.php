fa086aa4325590fd3a50c9d9595cde8df287986a
acipi <haris.tsipis@gmail.com>
2021-03-25 19:45:31 +0200

    finished services and technology stack

--- php/contact-form.php
+++ php/contact-form.php
@@ -20,17 +20,17 @@ require 'php-mailer/src/SMTP.php';
 require 'php-mailer/src/Exception.php';
 
 // Step 1 - Enter your email address below.
-$email = 'you@domain.com';
+$email = 'cipitech.com@gmail.com';
 
 // If the e-mail is not working, change the debug option to 2 | $debug = 2;
 $debug = 0;
 
 // If contact form don't has the subject input change the value of subject here
-$subject = ( isset($_POST['subject']) ) ? $_POST['subject'] : 'Define subject in php/contact-form.php line 29';
+$subject = (isset($_POST['subject'])) ? $_POST['subject'] : '[www.cipitech.com] New Message From Contact Form';
 
 $message = '';
 
-foreach($_POST as $label => $value) {
+foreach ($_POST as $label => $value) {
 	$label = ucwords($label);
 
 	// Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
@@ -40,12 +40,12 @@ foreach($_POST as $label => $value) {
 	// }
 
 	// Checkboxes
-	if( is_array($value) ) {
+	if (is_array($value)) {
 		// Store new value
 		$value = implode(', ', $value);
 	}
 
-	$message .= $label.": " . htmlspecialchars($value, ENT_QUOTES) . "<br>\n";
+	$message .= $label . ": " . htmlspecialchars($value, ENT_QUOTES) . "<br>\n";
 }
 
 $mail = new PHPMailer(true);
@@ -56,26 +56,26 @@ try {
 
 	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
 
-	//$mail->IsSMTP();                                         // Set mailer to use SMTP
-	//$mail->Host = 'mail.yourserver.com';				       // Specify main and backup server
-	//$mail->SMTPAuth = true;                                  // Enable SMTP authentication
-	//$mail->Username = 'user@example.com';                    // SMTP username
-	//$mail->Password = 'secret';                              // SMTP password
-	//$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
-	//$mail->Port = 587;   								       // TCP port to connect to
+	$mail->IsSMTP();                                         // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';				       // Specify main and backup server
+	$mail->SMTPAuth = true;                                  // Enable SMTP authentication
+	$mail->Username = 'cipitech.com@gmail.com';                    // SMTP username
+	$mail->Password = '';                              // SMTP password
+	$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
+	$mail->Port = 587;   								       // TCP port to connect to
 
 	$mail->AddAddress($email);	 						       // Add another recipient
 
-	//$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
+	$mail->AddAddress('haris.tsipis@gmail.com', 'Altin Cipi');     // Add a secondary recipient
 	//$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
 	//$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
 
 	// From - Name
-	$fromName = ( isset($_POST['name']) ) ? $_POST['name'] : 'Website User';
+	$fromName = (isset($_POST['name'])) ? $_POST['name'] : 'Website User';
 	$mail->SetFrom($email, $fromName);
 
 	// Repply To
-	if( isset($_POST['email']) ) {
+	if (isset($_POST['email'])) {
 		$mail->AddReplyTo($_POST['email'], $fromName);
 	}
 
@@ -87,12 +87,11 @@ try {
 	$mail->Body    = $message;
 
 	$mail->Send();
-	$arrResult = array ('response'=>'success');
-
+	$arrResult = array('response' => 'success');
 } catch (Exception $e) {
-	$arrResult = array ('response'=>'error','errorMessage'=>$e->errorMessage());
+	$arrResult = array('response' => 'error', 'errorMessage' => $e->errorMessage());
 } catch (\Exception $e) {
-	$arrResult = array ('response'=>'error','errorMessage'=>$e->getMessage());
+	$arrResult = array('response' => 'error', 'errorMessage' => $e->getMessage());
 }
 
 if ($debug == 0) {
